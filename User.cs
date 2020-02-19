d54849b615f21e1d90330a0c4d66fff480662f24
Jordan Parsons <49204159+jordanparsons9925@users.noreply.github.com>
2020-02-18 21:24:11 -0330

    wrote user class

--- User.cs
+++ User.cs
@@ -6,7 +6,153 @@ using System.Threading.Tasks;
 
 namespace Step4Prototype
 {
-    class User
+    public class User
     {
+        // the fields of the User class
+        private String userName;
+        private String passWord;
+        private String firstName;
+        private String lastName;
+        private String address;
+        private String city;
+        private String province;
+        private String postalCode;
+        private String homePhone;
+        private String cellPhone;
+        private String sin;
+        private DateTime dateOfBirth;
+        private String region;
+
+        // the constructor of the User class
+        public User()
+        {
+            userName = "";
+            passWord = "";
+            firstName = "";
+            lastName = "";
+            address = "";
+            city = "";
+            province = "";
+            postalCode = "";
+            homePhone = "";
+            cellPhone = "";
+            SIN = "";
+            dateOfBirth = "";
+            region = "";
+        }
+
+        // the properties of the User class
+        public String Username
+        {
+            get { return userName; }
+            set {
+                if (value.Length <= 50)
+                    userName = value;
+            }
+        }
+        public String Password
+        {
+            get { return passWord; }
+            set
+            {
+                if (value.Length <= 50)
+                    passWord = value;
+            }
+        }
+        public String Firstname
+        {
+            get { return firstName; }
+            set
+            {
+                if (value.Length <= 50)
+                    firstName = value;
+            }
+        }
+        public String Lastname
+        {
+            get { return lastName; }
+            set
+            {
+                if (value.Length <= 50)
+                    lastName = value;
+            }
+        }
+        public String Address
+        {
+            get { return address; }
+            set
+            {
+                if (value.Length <= 50)
+                    address = value;
+            }
+        }
+        public String City
+        {
+            get { return city; }
+            set
+            {
+                if (value.Length <= 50)
+                    city = value;
+            }
+        }
+        public String Province
+        {
+            get { return province; }
+            set
+            {
+                if (value.Length == 2)
+                    province = value;
+            }
+        }
+        public String Postalcode
+        {
+            get { return postalCode; }
+            set
+            {
+                if (value.Length == 6)
+                    postalCode = value;
+            }
+        }
+        public String Homephone
+        {
+            get { return homePhone; }
+            set
+            {
+                if (value.Length <= 10)
+                    homePhone = value;
+            }
+        }
+        public String Cellphone
+        {
+            get { return cellPhone; }
+            set
+            {
+                if (value.Length <= 10)
+                    cellPhone = value;
+            }
+        }
+        public String SIN
+        {
+            get { return sin; }
+            set
+            {
+                if (value.Length <= 9)
+                    sin = value;
+            }
+        }
+        public DateTime DOB
+        {
+            get { return dateOfBirth; }
+            set { dateOfBirth = value; }
+        }
+        public String Region
+        {
+            get { return region; }
+            set
+            {
+                if (value.Length <= 2)
+                    region = value;
+            }
+        }
     }
 }
