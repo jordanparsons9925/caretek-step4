119dc34218e6676e786700ca82863bdcff81b4bf
wonsbro <shineonworld@hotmail.com>
2020-02-19 18:46:56 -0330

    Update Service.cs

--- Service.cs
+++ Service.cs
@@ -6,24 +6,9 @@ namespace Step4Prototype
     {
         string[] services = { "Personal Care", "Light Housekeeping", "Social Interaction", "Respite" };
 
-        public string getService(string serviceCode)
+        public string getService(int i)
         {
-            switch (serviceCode)
-            {
-                case "PC":
-                    serviceName = "Personal Care";
-                    break;
-                case "LH":
-                    serviceName = "Light Housekeeping";
-                    break;
-                case "SI":
-                    serviceName = "Social Interaction";
-                    break;
-                case "R":
-                    serviceName = "Respite";
-                    break;
-            }
-            return serviceName;
+            return services[i];
         }
     }
 }
