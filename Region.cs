ae1d89b4f10fef0ca031548999cafbc1b348e7e5
wonsbro <shineonworld@hotmail.com>
2020-02-19 10:40:21 -0330

    Created Region class

---
+++ Region.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Step4Prototype
+{
+	static class Region
+	{
+		static string[] region = { "East Coast", "Central", "West Coast", "Northern Penninsula" };
+
+		public static Array Region
+		{
+			get
+			{
+				return region;
+			}
+
+		}
+	}
+}
