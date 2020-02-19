8f471d9c6a3e9766349516050376fbd87ce59dd4
wonsbro <shineonworld@hotmail.com>
2020-02-19 12:53:42 -0330

    Update Region.cs
    Change GetRegion to use switch case with regionCode and return regionName

--- Region.cs
+++ Region.cs
@@ -6,9 +6,25 @@ namespace Step4Prototype
     {
         static string[] regions = { "East Coast", "Central", "West Coast", "Northern Penninsula" };
 
-        public string GetRegion(int i)
+        public string GetRegion(string regionCode)
         {
-            return regions[i];
+            switch (regionCode)
+            {
+                case "EC":
+                    regionName = regions[0];
+                    break;
+                case "C":
+                    regionName = regions[1];
+                    break;
+                case "WC":
+                    regionName = regions[2];
+                    break;
+                case "NP":
+                    regionName = regions[3];
+                    break;
+            }
+
+            return regionName;
         }
     }
 }
