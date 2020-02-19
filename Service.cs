13203d64507b40ec4d7e4ddca13846b229c21278
wonsbro <shineonworld@hotmail.com>
2020-02-19 13:30:00 -0330

    Create Service.cs

---
+++ Service.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Step4Prototype
+{
+    class Service
+    {
+        string[] services = { "Personal Care", "Light Housekeeping", "Social Interaction", "Respite" };
+
+        public string getService(string serviceCode)
+        {
+            switch (serviceCode)
+            {
+                case "PC":
+                    serviceName = "Personal Care";
+                    break;
+                case "LH":
+                    serviceName = "Light Housekeeping";
+                    break;
+                case "SI":
+                    serviceName = "Social Interaction";
+                    break;
+                case "R":
+                    serviceName = "Respite";
+                    break;
+            }
+            return serviceName;
+        }
+    }
+}
