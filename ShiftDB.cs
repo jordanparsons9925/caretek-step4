f28a7fda7a7c9e1693a33fa21b0c68edcbeb435d
Hai The Dude <hainghiem375@gmail.com>
2020-02-25 15:26:57 -0330

    DB Classes

--- ShiftDB.cs
+++ ShiftDB.cs
@@ -10,6 +10,47 @@ namespace Step4Prototype
 {
     class ShiftDB
     {
+        public static PCA GetPCA(string username)
+        {
+            SqlConnection connection = CareBase.GetConnection();
+            string selectStatement
+                = "SELECT Username, FirstName, FirstName, Address, City, State, PostalCode, PositionID "
+                + "FROM Employees "
+                + "WHERE username = @username";
+            SqlCommand selectCommand =
+                new SqlCommand(selectStatement, connection);
+            selectCommand.Parameters.AddWithValue("@username", username);
 
+            try
+            {
+                connection.Open();
+                SqlDataReader PCAReader =
+                    selectCommand.ExecuteReader(CommandBehavior.SingleRow);
+                if (pcaReader.Read())
+                {
+                    PCA pca = new PCA();
+                    pca.username = (string)pcaReader["username"];
+                    pca.FirstName = pcaReader["FirstName"].ToString();
+                    pca.LastName = pcaReader["LastName"].ToString();
+                    pca.Address = pcaReader["Address"].ToString();
+                    pca.City = pcaReader["City"].ToString();
+                    pca.PostalCode = pcaReader["PostalCode"].ToString();
+                    pca.PositionID = pcaReader["PositionID"].ToString();
+                    return pca;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
     }
 }
